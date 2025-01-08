987d2b58b8ea165cf1f8d0ae070127a7c674a507
osman-yasser <osmanyasseradel77@gmail.com>
2025-01-09 00:18:48 +0200

    Implement Late Entry & Early Exit Rules First Phase

---
+++ attendance/attendance/doctype/late_entry_early_exit_child_table/late_entry_early_exit_child_table.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Peter Maged and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class LateEntryEarlyExitChildTable(Document):
+	pass
